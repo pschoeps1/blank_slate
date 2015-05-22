9145c892bdfedb260f356e95d70613e839f19d62
Patrick Schoeps <pschoeps@umich.edu>
2015-05-22 17:49:16 +0000

    final merge

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,24 @@
 class GroupsController < ApplicationController
   #  before_action :logged_in_user, only: [:create, :destroy]
 
+  def new
+    @user = User.find(params[:user_id])
+    @group = current_user.groups.build
+  end
+  
+  def show
+    @user = User.find(params[:user_id])
+    @group_show = Group.find(params[:id])
+  end
+  
   def create
+    @user = User.find(params[:user_id])
     @group = current_user.groups.build(group_params)
     if @group.save
       flash[:success] = "Class created!"
-      redirect_to @group
+      redirect_to @user
     else
-      render 'static_pages/home'
+      render 'new'
     end
   end
 
